01d0c30869ab7451a8bfe34ec2be09584f02459c
Calvin <calmoon808@gmail.com>
2019-07-03 18:30:33 -1000

    for real now

--- multiples.js
+++ multiples.js
@@ -8,9 +8,7 @@ exports.sumOfAMultiple = function( n ) {
 
   // do your work here
   for (let i = 0; i < n; i++){
-    if (i % 3 === 0){
-      sum += i;
-    } else if (i % 5 === 0){
+    if (i % 3 === 0 || i % 5 === 0){
       sum += i;
     }
   }
