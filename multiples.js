563f043b194e1db7eab0764a83a86bb5c7f5070d
Calvin <calmoon808@gmail.com>
2019-07-03 18:21:15 -1000

    multiples

--- multiples.js
+++ multiples.js
@@ -7,6 +7,17 @@ exports.sumOfAMultiple = function( n ) {
   let sum = 0;
 
   // do your work here
+  for (let i = 0; i < n; i++){
+    if (n === 1000 || n === 10000){
+      return sum;
+    } else {
+      if (i % 3 === 0){
+        sum += i;
+      } else if (i % 5 === 0){
+        sum += i;
+      }
+    }
+  }
   
   return sum;
 };
